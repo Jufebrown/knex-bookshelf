a876da43f37879d584b06dc930437891d34411de
Jufebrown <jufebrown@gmail.com>
2017-04-03 15:06:51 -0500

    added monsters and battles models

---
+++ models/monsters.js
@@ -0,0 +1,15 @@
+const {bookshelf} = require('../bookshelf.js');
+
+require('./battles');
+const Monster = bookshelf.Model.extend({
+  tableName : 'monsters',
+  idAttribute: 'monster_id',
+  battles: function() {
+    //The hasMany relation specifies that this model has one or more
+    //rows in  another table which match on this model's primary key.
+    return this.hasMany('Battle', 'monster_id')
+  }
+})
+
+
+module.exports = bookshelf.model('Monster', Monster)
