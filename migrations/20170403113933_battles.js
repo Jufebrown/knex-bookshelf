99de5071c3efa0a87af56fce06ccc280a5dc49e2
Jufebrown <jufebrown@gmail.com>
2017-04-03 11:45:55 -0500

    defined battles schema

--- migrations/20170403113933_battles.js
+++ migrations/20170403113933_battles.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('battles', (table) => {
+      table.increments('battle_id').primary()
+      table.string('battle_location').notNullable()
+      table.foreign('monster_id')
+      table.foreign('hero_id')
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('battles')
 };
