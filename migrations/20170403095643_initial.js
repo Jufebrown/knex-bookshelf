d41a65720468d1ec7e1c2f27d5f8d5e46120041b
Jufebrown <jufebrown@gmail.com>
2017-04-03 10:14:27 -0500

    create and drop table

--- migrations/20170403095643_initial.js
+++ migrations/20170403095643_initial.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('monsters', (table) => {
+      table.increments('monster_id').primary()
+      table.string('monster_name').notNullable()
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('monsters')
 };
