ed40af98804d80bdaf56063a6d2eff86e8960918
Jufebrown <jufebrown@gmail.com>
2017-04-03 10:52:57 -0500

    added functions to create and drop heroes table

--- migrations/20170403104804_heroes.js
+++ migrations/20170403104804_heroes.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('heroes', (table) => {
+      table.increments('hero_id').primary()
+      table.string('hero_name').notNullable()
+      table.
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('heroes')
 };
