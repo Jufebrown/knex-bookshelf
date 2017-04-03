ec20c9af2371accbe3c00d3e45ffefad7a74a223
Jufebrown <jufebrown@gmail.com>
2017-04-03 15:44:50 -0500

    added a collection of monsters

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const {bookshelf} = require('./bookshelf')
 const Battle = require('./models/battles')
 const Monster = require('./models/monsters')
 
-const monster = new Monster()
-monster.set('monster_name', 'Sully')
+// const monster = new Monster()
+// monster.set('monster_name', 'Sully')
 
-monster.save().then((m) => {
-  console.log('Monster saved:', m.get(monster_name))
-})
+// monster.save().then((m) => {
+//   console.log('Monster saved:', m.get(monster_name))
+// })
 
-Battle.byLocation('Hoth').then(() => {
-  console.log('Got Battle: ', battle.get('monster_id'), battle.get('hero_id'))
-})
+// Battle.byLocation('Hoth').then(() => {
+//   console.log('Got Battle: ', battle.get('monster_id'), battle.get('hero_id'))
+// })
 
 Monster.forge({monster_name: 'Cookie'}).fetch({withRelated: ['battles']})
 .then((monster) => {
   console.log('Got Monster:', monster.get('monster_name'), monster.get('monster_id'))
   console.log('Got Battles:', monster.related('battles').toJSON())
+})
+
+const Monsters = bookshelf.Collection.extend({
+  model: Monster
+})
+
+Monsters.forge().fetch().then((monsters) => {
+  console.log('monsters', monsters.toJSON())
 })
