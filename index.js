a4a383658a8dc226c424b4737701d36b9bf759ad
Jufebrown <jufebrown@gmail.com>
2017-04-03 15:31:34 -0500

    made query to find battles

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const {bookshelf} = require('./bookshelf')
 
 const Battle = require('./models/battles')
-const Monster = require('./models/battles')
+const Monster = require('./models/monsters')
 
 const monster = new Monster()
 monster.set('monster_name', 'Sully')
 
 monster.save().then((m) => {
-  console.log('Monster saved:', m.get('monster_name'))
+  console.log('Monster saved:', m.get(monster_name))
+})
+
+Battle.byLocation('Hoth').then(() => {
+  console.log('Got Battle: ', battle.get('monster_id'), battle.get('hero_id'))
 })
