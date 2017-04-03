506d13e48bff2882eba27d22a4f51d102a8bcccf
Jufebrown <jufebrown@gmail.com>
2017-04-03 15:06:27 -0500

    added bookshelf.js

---
+++ bookshelf.js
@@ -0,0 +1,7 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require('./knexfile.js');
+const knex = require('knex')(config[env]);
+const bookshelf = require('bookshelf')(knex);
+
+
+bookshelf.plugin('registry');
