80736fee6a251fef54b96badb0912919d53e53ea
Jufebrown <jufebrown@gmail.com>
2017-04-03 15:39:30 -0500

    added query to find battles with Cookie monster

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ monster.save().then((m) => {
 Battle.byLocation('Hoth').then(() => {
   console.log('Got Battle: ', battle.get('monster_id'), battle.get('hero_id'))
 })
+
+Monster.forge({monster_name: 'Cookie'}).fetch({withRelated: ['battles']})
+.then((monster) => {
+  console.log('Got Monster:', monster.get('monster_name'), monster.get('monster_id'))
+  console.log('Got Battles:', monster.related('battles').toJSON())
+})
